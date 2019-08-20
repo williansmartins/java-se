d8f2b4dfb39415f01dd01d500e2169f85f1a9841
Willians Martins de Morais - Instrutor <Willians@classroom.net>
2019-08-20 11:47:43 -0300

    refactor

---
+++ desafios/desafio4/src/main/java/com/williansmartins/desafio3/App.java
@@ -0,0 +1,13 @@
+package com.williansmartins.desafio3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Desafio 3!" );
+    }
+}
