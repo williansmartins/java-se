c7396cb67bfe58fa387d51d06e3bcd9890e2e0b8
Martins de Morais <wsms@gft.com>
2019-11-01 13:49:37 -0300

    add exemplos

---
+++ projeto-exemplos/src/main/java/com/williansmartins/a/estruturaDeClasse/Argumentos.java
@@ -0,0 +1,11 @@
+package com.williansmartins.a.estruturaDeClasse;
+
+public class Argumentos {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < args.length; i++) {
+			String argumento = args[i];
+			System.out.println(argumento);
+		}
+	}
+}
